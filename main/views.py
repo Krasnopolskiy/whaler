5bdad2631b6d768e2f9f22f566b9bb0d1ee81f8c
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-08-06 17:10:18 +0300

    Add reports

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -8,7 +10,6 @@ from main.heuristics.checker import Checker
 from main.heuristics.database import DatabaseHeuristic
 from main.heuristics.google_safebrowsing import GoogleSafeBrowsingHeuristic
 from main.heuristics.yandex_safebrowsing import YandexSafeBrowsingHeuristic
-
 
 CHECKER = Checker(
     [
@@ -24,7 +25,7 @@ class IndexView(View):
     context = {}
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'index.html')
+        return render(request, 'index.html', self.context)
 
     def post(self, request: HttpRequest) -> HttpResponse:
         address = request.POST.get('address')
@@ -33,3 +34,10 @@ class IndexView(View):
         self.context['result'] = CHECKER.process(address)
         self.context['address'] = address
         return render(request, 'index.html', self.context)
+
+@method_decorator(staff_member_required, name='dispatch')
+class ReportsView(View):
+    context = {}
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'report.html', self.context)
--- whaler/urls.py
+++ whaler/urls.py
@@ -1,6 +1,9 @@
-from main.views import IndexView
+from main.views import IndexView, ReportsView
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
+    path('reports/', ReportsView.as_view(), name='reports'),
 ]
